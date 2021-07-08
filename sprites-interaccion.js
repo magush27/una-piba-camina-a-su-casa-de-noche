d9998f205a76910898c887169ceac726d3e55a72
magush27 <aguslasp@gmail.com>
2021-07-08 20:49:45 -0300

    agrego gifs, arreglo responsive y diseño

---
+++ sprites-interaccion.js
@@ -0,0 +1,75 @@
+$("#gif1").hover(
+    function() {
+        $(this).attr("src", "/img/sprite/defengochi.gif");
+    },
+    function() {
+        $(this).attr("src", "/img/sprite/defengochi1.png");
+    }                         
+);  
+
+$("#gif2").hover(
+    function() {
+        $(this).attr("src", "/img/sprite/barril.gif");
+    },
+    function() {
+        $(this).attr("src", "/img/sprite/barril1.png");
+    }                         
+);
+
+$("#gif3").hover(
+    function() {
+        $(this).attr("src", "/img/sprite/drone.gif");
+    },
+    function() {
+        $(this).attr("src", "/img/sprite/drone1.png");
+    }                         
+);
+
+
+$("#gif4").hover(
+    function() {
+        $(this).attr("src", "/img/sprite/lucero.gif");
+    },
+    function() {
+        $(this).attr("src", "/img/sprite/lucero1.png");
+    }                         
+);
+
+$("#gif1").click(
+    function() {
+        $(this).attr("src", "/img/sprite/defengochi.gif");
+    },
+    function() {
+        $(this).attr("src", "/img/sprite/defengochi1.png");
+    }                         
+);  
+
+$("#gif2").click(
+    function() {
+        $(this).attr("src", "/img/sprite/barril.gif");
+    },
+    function() {
+        $(this).attr("src", "/img/sprite/barril1.png");
+    }                         
+);
+
+$("#gif3").click(
+    function() {
+        $(this).attr("src", "/img/sprite/drone.gif");
+    },
+    function() {
+        $(this).attr("src", "/img/sprite/drone1.png");
+    }                         
+);
+
+
+$("#gif4").click(
+    function() {
+        $(this).attr("src", "/img/sprite/lucero.gif");
+    },
+    function() {
+        $(this).attr("src", "/img/sprite/lucero1.png");
+    }                         
+);
+
+
